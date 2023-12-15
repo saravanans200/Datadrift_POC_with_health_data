426fd2bcc7bca91e05c9bfe5351b30f59d232b0a
Saravana Kumar Shanmuganathan <saravana.shanmuganathan@axtria.com>
2023-12-15 17:04:47 +0530

    data update saravana 15-DEC-2023 15.04PM

--- datacrawler.py
+++ datacrawler.py
@@ -5,14 +5,14 @@ import time
 
 print("started")
 def func(): 
-    print("Geeksforgeeks")
+    print("started")
     filename = 'src/result.ipynb'
     with open(filename) as ff:
         nb_in = nbformat.read(ff, nbformat.NO_CONVERT)    
     ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
     nb_out = ep.preprocess(nb_in)
   
-schedule.every(30).minutes.do(func) 
+schedule.every(1).minutes.do(func) 
   
 while True: 
     schedule.run_pending() 
